f05ae423a6a5b5127fd2d58168290128d140fe59
Marko Marinovic <markoman.marko@gmail.com>
2018-07-23 12:13:17 +0200

    add new token

--- src/reducers/tokensReducer.js
+++ src/reducers/tokensReducer.js
@@ -1,16 +1,34 @@
 import _ from 'underscore';
-import { LOAD_TOKENS, LOAD_TOKEN_BALANCE } from '../actions/tokenActions';
+import { LOAD_TOKENS, LOAD_TOKEN_BALANCE, ADD_TOKEN } from '../actions/tokenActions';
 
 export default function(state = [], action){
     switch(action.type){
         case LOAD_TOKENS:
             return action.payload.data.tokens;
-        case LOAD_TOKEN_BALANCE:
-            const newState = [...state];
-            const token = _.findWhere(newState, { address: action.payload.contractAddress});
-            token.balance = action.payload.balance;
-            return newState;
+        case LOAD_TOKEN_BALANCE:    
+            return _loadTokenBalance(state, action);
+        case ADD_TOKEN:
+            return _addToken(state, action);
     }
 
     return state;
+}
+
+function _loadTokenBalance(state, action){
+    const newState = [...state];
+    const token = _.findWhere(newState, { address: action.payload.contractAddress});
+    token.balance = action.payload.balance;
+    return newState;
+}
+
+function _addToken(state, action){
+    const newState = [...state];
+    const token = _.findWhere(newState, { address: action.token.contractAddress });
+    if(token){
+        token.symbol = action.token.symbol;
+        token.contractAddress = action.token.contractAddress;
+    }else{
+        newState.push({ symbol: action.token.symbol, address: action.token.contractAddress });
+    }
+    return newState;
 }
